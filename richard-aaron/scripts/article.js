576ae005020719d3f4bc97f0b375ba9c74c9dc6e
richjimdev <richjimdev@gmail.com>
2018-09-07 10:10:33 -0700

    fixed bug with categories, handlebars working

--- richard-aaron/scripts/article.js
+++ richard-aaron/scripts/article.js
@@ -11,23 +11,26 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
+let source   = document.getElementById("article-template").innerHTML;
+let articleTemplate = Handlebars.compile(source);
+
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  //DONE ** TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
 
-  $newArticle.attr('data-author', this.author);
-  $newArticle.find('address a').html(this.author);
-  $newArticle.find('address a').attr('href', this.authorUrl);
-  $newArticle.find('h1:first').html(this.title);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('time').attr('datetime', this.publishedOn);
-  $newArticle.find('time').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newArticle.append('<hr>');
+  // $newArticle.attr('data-author', this.author);
+  // $newArticle.find('address a').html(this.author);
+  // $newArticle.find('address a').attr('href', this.authorUrl);
+  // $newArticle.find('h1:first').html(this.title);
+  // $newArticle.find('.article-body').html(this.body);
+  // $newArticle.find('time').attr('datetime', this.publishedOn);
+  // $newArticle.find('time').attr('title', this.publishedOn);
+  // $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  // $newArticle.append('<hr>');
 
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
@@ -42,8 +45,8 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  //DONE ** TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return articleTemplate(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
